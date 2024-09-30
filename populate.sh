f20dd217d7215cd8226e38419b0c18087c43d629
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-09-30 14:52:22 +0200

    fix: update directus path in scripts to be agnostic

--- populate.sh
+++ populate.sh
@@ -1,4 +1,6 @@
-docker exec -i clic-postgres psql --user directus_user directus_data < ./directus/dump.sql
+DIR=$(dirname $0)
+
+docker exec -i clic-postgres psql --user directus_user directus_data < $DIR/dump.sql
 
 # Restarts directus to reload the permission table
 docker restart clic-directus
